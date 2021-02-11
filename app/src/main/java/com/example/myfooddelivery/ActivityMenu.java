1240b55fa2da58f4f87e9bb9e49f5df7b56f2545
HarutKloyan <h.kloyan@mail.ru>
2021-02-11 22:53:23 +0400

    New code

--- app/src/main/java/com/example/myfooddelivery/ActivityMenu.java
+++ app/src/main/java/com/example/myfooddelivery/ActivityMenu.java
@@ -12,6 +12,5 @@ public class ActivityMenu extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
     }
 }
--- app/src/main/java/com/example/myfooddelivery/MainActivity.java
+++ app/src/main/java/com/example/myfooddelivery/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.myfooddelivery;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.List;
 
@@ -18,11 +24,23 @@ public class MainActivity extends AppCompatActivity {
     PopularAdapter popularAdapter;
     RecommendedAdapter recommendedAdapter;
     AllMenuAdapter allMenuAdapter;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.menu_nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
     public void OnClickMenu(View view) {
         startActivity(new Intent(this, ActivityMenu.class));
