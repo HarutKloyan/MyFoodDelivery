26638088b11271844929d81b3520c87f84ceb79b
HarutKloyan <h.kloyan@mail.ru>
2021-02-24 21:36:43 +0400

    Закончен StartAct начата анимация LoginAct

--- app/src/main/java/com/example/myfooddelivery/StartActivity.java
+++ app/src/main/java/com/example/myfooddelivery/StartActivity.java
@@ -2,13 +2,57 @@ package com.example.myfooddelivery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class StartActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 2000;
+
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView logo, slogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_start);
+
+        // Animation
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_anim);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_anim);
+
+        // Hooks
+        image = findViewById(R.id.logoImage_start);
+        logo = findViewById(R.id.logo_name_start);
+        slogan = findViewById(R.id.slogan_name_start);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(StartActivity.this,LoginActivity.class);
+
+                Pair[] pairs = new Pair[2];
+                pairs[0] = new Pair<View,String>(image, "logo_image");
+                pairs[1] = new Pair<View,String>(logo, "logo_txt");
+
+                ActivityOptions options = new ActivityOptions.makeSceneTransitionAnimation(StartActivity.this, pairs);
+                startActivity(intent, options.toBundle());
+            }
+        },SPLASH_SCREEN);
     }
 }
